0c03d6cf58db891809b6893edc08964a01cb5a19
Kalp9403 <kalppandya94@gmail.com>
2023-07-03 14:32:02 +0530

    Connected to PostgreSQL and added and fetch data from it

--- travello/views.py
+++ travello/views.py
@@ -3,27 +3,6 @@ from .models import Destination
 # Create your views here.
 
 def index(request):
-    dest1 = Destination()
-    dest1.name = 'Mumbai'
-    dest1.img = 'destination_1.jpg'
-    dest1.desc = 'The City That Never Sleeps'
-    dest1.price = 700
-    dest1.offer = False
 
-    dest2 = Destination()
-    dest2.name = 'Ahmedabad'
-    dest2.img = 'destination_2.jpg'
-    dest2.desc = 'Sweet Home'
-    dest2.price = 800
-    dest2.offer = False
-
-    dest3 = Destination()
-    dest3.name = 'Gandhinagar'
-    dest3.img = 'destination_3.jpg'
-    dest3.desc = 'Little Brother of Chandigadh'
-    dest3.price = 650
-    dest3.offer = True
-
-    dests = [dest1, dest2, dest3]
-
+    dests = Destination.objects.all()    
     return render(request, 'index.html', {'dests': dests})
